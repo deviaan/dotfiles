1d44a6bf82532576249f1aa3df518d438f1e3449
deviaan <deviaan@pop-os.localdomain>
2025-09-09 15:52:23 -0600

    minor change~

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,6 +26,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = { { import = "plugins" } }, -- import plugins
-	install = { colorscheme = { "habamax" } }, -- colorscheme loading
+	install = { colorscheme = { "tokyonight" } }, -- colorscheme loading
 	checker = { enabled = true }, -- auto update plugins
 })
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,5 +8,6 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font('Hack Nerd Font')
 config.font_size = 12.0
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
