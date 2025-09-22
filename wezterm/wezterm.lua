96e949e96de390d053a53b48ba3699c58f213758
deviaan <deviaan2@gmail.com>
2025-09-22 14:59:28 -0600

    plugin updates & remove window decorations from wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,5 +9,6 @@ config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font('Hack Nerd Font')
 config.font_size = 12.0
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "None"
 
 return config
